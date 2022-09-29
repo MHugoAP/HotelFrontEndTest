121e02fab7ac4d4df2161cb14ccc097848bd8b4f
Miguel Hugo <dragonmike6@gmail.com>
2022-09-29 10:27:49 -0500

    we create the constructors according to what is needed in the feature

---
+++ hotelFrontEndTest/src/main/java/co/speedup/qa/model/HomeModel.java
@@ -0,0 +1,25 @@
+package co.speedup.qa.model;
+
+public class HomeModel {
+    private String location;
+    private String checkIn;
+    private String checkOut;
+
+    public HomeModel(String location, String checkIn, String checkOut) {
+        this.location = location;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+}
